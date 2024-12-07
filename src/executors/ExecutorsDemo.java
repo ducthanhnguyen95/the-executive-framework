9b824df7fedb106ab3cad0a5384819cb9123ff38
thanhnd <ducthanhnguyen95@gmail.com>
2024-12-07 14:59:57 +0700

    Executors

---
+++ src/executors/ExecutorsDemo.java
@@ -0,0 +1,18 @@
+package executors;
+
+import java.util.concurrent.Executors;
+
+public class ExecutorsDemo {
+
+    public static void show() {
+        var executor = Executors.newFixedThreadPool(2);
+
+        try {
+            executor.submit(() -> {
+                System.out.println(Thread.currentThread().getName());
+            });
+        } finally {
+            executor.shutdown();
+        }
+    }
+}
